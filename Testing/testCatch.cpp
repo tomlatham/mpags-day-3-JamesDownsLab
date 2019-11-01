0f299ebbfef2681d0e84fd9be6e05132236f3281
James Downs <jamesdownslab@gmail.com>
2019-11-01 19:31:22 +0000

    Added fake test

---
+++ Testing/testCatch.cpp
@@ -0,0 +1,8 @@
+//! Test that catch works
+#define CATCH_CONFIG_MAIN
+
+#include "catch.hpp"
+
+TEST_CASE("Addition works", "[math]"){
+    REQUIRE(1 + 1 == 2);
+}
--- Testing/testHello.cpp
+++
@@ -1,8 +0,0 @@
-//
-// Created by james on 01/11/2019.
-//
-#include <iostream>
-
-int main(){
-    std::cout << "Hello World!" << std::endl;
-}
