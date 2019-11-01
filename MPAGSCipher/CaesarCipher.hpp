d38cb8806808b1cb91e84ddf39a7ce68418f6b19
James Downs <jamesdownslab@gmail.com>
2019-11-01 14:35:47 +0000

    made key_ private

--- MPAGSCipher/CaesarCipher.hpp
+++ MPAGSCipher/CaesarCipher.hpp
@@ -14,13 +14,12 @@ class CaesarCipher {
         explicit CaesarCipher(const size_t key);
         explicit CaesarCipher(const std::string& key);
 
-        size_t key_;
-
         std::string applyCipher(const std::string& inputText, const bool encrypt) const;
 
     private:
         const std::vector<char> alphabet_ = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
         const size_t alphabetSize_ = alphabet_.size();
+        size_t key_;
 };
 
 
